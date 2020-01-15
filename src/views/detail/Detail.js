58ccd4d18fbeb832ac4ebcbc932823ee00ad5c22
Luis Cazzullo <lcazzullo@gmail.com>
2020-01-14 23:14:01 -0300

    Aside was added

--- src/views/detail/Detail.js
+++ src/views/detail/Detail.js
@@ -5,6 +5,7 @@ import { get } from '../../utils/services';
 // Components
 import Comment from '../../components/comment/Comment';
 import Carrousel from '../../components/carrousel/Carrousel';
+import Aside from '../../components/aside/Aside';
 
 class Detail extends Component {
     constructor(props) {
@@ -53,7 +54,14 @@ class Detail extends Component {
                             {description}
                         </div>
                         <div className="col-3">
-                            Aside
+                            <Aside 
+                                items={[
+                                    {
+                                        title: title,
+                                        img: image
+                                    }
+                                ]}
+                            />
                         </div>
                     </div>
                     <div className="row">
